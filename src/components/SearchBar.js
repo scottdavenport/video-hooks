90d58b6e9fb6bb0fcf80a9c6be5b0af9369bafc1
scottdavenport <scott@pinpoint.com>
2021-01-12 21:50:49 -0500

    Small Change for Deploy

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -20,7 +20,7 @@ class SearchBar extends React.Component {
 			<div className="search-bar ui segment">
 				<form onSubmit={this.onFormSubmit} className="ui form">
 					<div className="field">
-						<label>Video Search</label>
+						<label>Search for a Video</label>
 						{/* Could also be... {e => this.setState({ term: e.target.value })} */}
 						<input type="text" value={this.state.term} onChange={this.onInputChange} />
 					</div>
